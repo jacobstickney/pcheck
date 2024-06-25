3f439a0e660157bb0d27a324507725855ceb1608
jacobstickney <86248382+jacobstickney@users.noreply.github.com>
2024-06-25 15:11:04 -0400

    Update pcheck.py

--- pcheck.py
+++ pcheck.py
@@ -1,7 +1,7 @@
-
 import requests
 import json
 import readline  # or import pyreadline for windows
+import getpass
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from pygments import highlight
 from pygments.lexers import JsonLexer
@@ -21,6 +21,10 @@ ascii_art = r"""
 
 description = """Your Personal Guard Against IP Masqueraders.
 Retrieves information about the IP including whether it's a proxy, VPN, its ASN and node information.
+\
+If there are multiple IPs, separate by comma.
+\n
+exit -- exits the script
 """
 
 def check_ips(api_key, ip_addresses):
@@ -37,5 +41,15 @@ print(ascii_art)
 print(Fore.CYAN + description + Style.RESET_ALL)
 
 api_key = '716550-605120-904905-941c40'
-ip_addresses = input("Please enter an IP addresses. If there are multiple IPs, separate by comma: ").strip().split(',')
-check_ips(api_key, ip_addresses)
+
+while True:
+    ip_input = input("Please enter an IP address: ").strip().lower()
+    if ip_input == 'exit':
+        print("Exiting...")
+        break
+    ip_addresses = ip_input.split(',')
+    check_ips(api_key, ip_addresses)
+    more_ips = getpass.getpass("More IPs to query? (y/n) ")
+    if more_ips.lower() != 'y':
+        print("Exiting...")
+        break
